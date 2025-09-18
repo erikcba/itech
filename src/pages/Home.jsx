5aec800fff4506a7e76c5906e1a99cdcb0c63c64
Erik Tomaselli <eriktomaselli97@gmail.com>
2025-09-18 17:16:26 -0300

    modificacion en width home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
   }]
 
   return (
-    <div className='p-6 container mx-auto flex flex-col gap-6'>
+    <div className='p-6 flex flex-col gap-6'>
       <h1 className='text-3xl font-bold mb-4'>
         Dashboard
       </h1>
